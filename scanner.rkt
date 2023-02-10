eddb1e76a40edcaac45a2d0abfa6939f750f950e
bpiotrowski154 <88128424+bpiotrowski154@users.noreply.github.com>
2023-02-10 17:18:45 -0600

    Scanner prototype 1
    The scanner function is now working. When parse is called and a file name is passed in, a list is created from the file. Then the scanner is called with that list being passed to it. The scanner first changes the datatype of every datum in the list to be of data type string. Then the scanner goes through each item in the list and reassigns it as a token for the parser.

--- scanner.rkt
+++ scanner.rkt
@@ -1,33 +1,88 @@
 #lang racket
 
 (define (fileAsList str)
-  (define temp (string->list(file->string str))) ;take in a file as a string and convert it into a list
-  (define temp3 (file->string str))
-  (define temp2 (string-split (string-trim (string-replace (file->string str) "\n" " ")) " "))
-  (println temp)
-  (println temp3)
-  (println temp2)
-  (removeSpaces temp) ;remove all the unnecessary whitespaces
+  ;(define temp (string-split (string-trim (string-replace (file->string str) "\n" " ")) " "))
+  ;(define temp (file->list str))
+  (file->list str)
+  ;(removeSpaces temp) ;remove all the unnecessary whitespaces
   )
 
 
 
-(define (removeSpaces lst) ;algorithm modified off of removesymbols algorithm from https://stackoverflow.com/questions/36928298/remove-symbols-from-list-scheme
-  (cond
-    [(null? lst) '()]
-    [(equal? (car lst) #\space) (removeSpaces (cdr lst))]
-    [(equal? (car lst) #\newline) (removeSpaces (cdr lst))]
-    [else (cons (car lst) (removeSpaces (cdr lst)))]))
-  
-
-;(define (flagger str)
+;(define (removeSpaces lst) ;algorithm modified off of removesymbols algorithm from https://stackoverflow.com/questions/36928298/remove-symbols-from-list-scheme
  ; (cond
-  ;  [(char-alphabetic? str) 'id]
+  ;  [(null? lst) '()]
+   ; [(equal? (car lst) #\space) (removeSpaces (cdr lst))]
+    ;[(equal? (car lst) #\newline) (removeSpaces (cdr lst))]
+    ;[else (cons (car lst) (removeSpaces (cdr lst)))]))
+  
+(define (string-alphabetic? str) ;Algorithm from discussion board post
+   (define (iter ch-list)
+       (if (empty? ch-list)
+          #t
+          (if (char-alphabetic? (first ch-list))
+              (iter (rest ch-list))
+              #f)))
+     (iter (string->list str)))
+
+
+(define (flagger str)
+  (if (string-alphabetic? str)
+      (cond
+        [(equal? str "if") 'if]
+        [(equal? str "then") 'then]
+        [(equal? str "goto") 'goto]
+        [(equal? str "gosub") 'gosub]
+        [(equal? str "return") 'return]
+        [(equal? str "read") 'read]
+        [(equal? str "write") 'write]
+        [else 'id])
+      (if (and (number? str)(positive? (string->number str)))
+          'idx
+          (cond
+            [(equal? str ":") 'colon]
+            [(equal? str "(") 'lparen]
+            [(equal? str ")") 'rparen]
+            [(equal? str "$$") '$$]
+            [(equal? str "+") 'plus]
+            [(equal? str "-") 'minus]
+            [(equal? str "=") 'equals]
+            [else 'err]))))
+
+;Test for number then alphabetic then symbol
+
+
+  ;(cond
+  ;  [(equal? str "if") 'if]
+  ;  [(equal? str "then") 'then]
+  ;  [(equal? str "goto") 'goto]
+  ;  [(equal? str "gosub") 'gosub]
+  ;  [(equal? str "return") 'return]
+  ;  [(equal? str "read") 'read]
+  ;  [(equal? str "write") 'write]
+  ;  [(equal? str ":") 'colon]
+  ;  [(equal? str "(") 'lparen]
+  ;  [(equal? str ")") 'rparen]
+  ;  [(equal? str "$$") '$$]
+  ;  [(equal? str "+") 'plus]
+  ;  [(equal? str "-") 'minus]
+   ; [(equal? str "=") 'equals]
+   ;; ;[(char-alphabetic? str) 'id]
+    ;[(positive? (string->number str)) 'idx]
+    ;[else 'err]))
+    
     
 
 (define (scanner tokens)
-  ;(println tokens)
-  (println "done")
+  tokens
+  (println tokens)
+  (set! tokens (map (lambda (x)
+         (if (number? x)
+             (number->string x)
+             (~a x))) tokens))
+  (println tokens)
+  (set! tokens (map flagger tokens))
+  (println tokens)
   )
 
 
@@ -37,6 +92,4 @@
 
 
 
-;(define keywords (fileAsList "file01.txt"))
-;(scanner keywords)
-(parse "file02.txt")
+(parse "file01.txt")
